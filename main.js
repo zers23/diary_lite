bfee5206c9783ec76578bca69a73d397b0068546
Grebenshchikov <zerset@yandex.ru>
2021-08-19 12:22:51 +0300

    bugfix (create db)

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const express = require("express"),
   port = 3000,
   sqlite3 = require("sqlite3").verbose(),
   db = new sqlite3.Database("notes.db"),
+  fs = require("fs");
   moment = require("moment");
-
+fs.openSync("notes.db","a+");
 app.listen(port);
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true, limit: "6mb" }));
-app.use(express.json({ limit: "6mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
   res.sendFile("index");
@@ -24,11 +25,17 @@ function addDataOnDB(data) {
   let time = moment().format("DD-MM-YYYY; HH:mm:ss");
   console.log("OK! " + time);
   db.serialize(function () {
+    db.run(
+      `CREATE TABLE IF NOT EXISTS "main" (
+        "date"	TEXT(35),
+        "head"	TEXT,
+        "body"	TEXT
+      );`
+    )
     db.each(
-      `INSERT INTO main ("date","head","body") VALUES ("${time}","${data.header}", "${data.data}")`,
-      (err, row) => {
-        db.close();
-      }
-    );
+      `INSERT INTO main ("date","head","body") 
+      VALUES 
+      ("${time}","${data.header}", "${data.data}")`
+      );
   });
 }
